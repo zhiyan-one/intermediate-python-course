9435f3a4b3bddd628f8b0733002c83515d28ce9f
zhiyan-one <55054852+zhiyan-one@users.noreply.github.com>
2020-05-19 16:03:58 +0800

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -6,13 +6,13 @@ def mian():
   for i in range(0,dice_rolls):
     roll = random.randint(1,dice_size)
     dice_sum += roll
-  if roll == 1:
-    print(f'You rolled a {roll}! Critical Fail')
-  elif roll == dice_size:
-    print(f'You rolled a {roll}! Critical Success!')
-  else:
-    print(f'You rolled a {roll}')
-    print(f'You have rolled a total of {dice_sum}')
+    if roll == 1:
+      print(f'You rolled a {roll}! Critical Fail')
+    elif roll == dice_size:
+      print(f'You rolled a {roll}! Critical Success!')
+    else:
+      print(f'You rolled a {roll}')
+      print(f'You have rolled a total of {dice_sum}')
 
 if __main__ = "__main__":
   main()
