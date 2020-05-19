194169d6de965ed09b82ff6c5fe1ded3e19204a4
zhiyan-one <55054852+zhiyan-one@users.noreply.github.com>
2020-05-19 15:15:32 +0800

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,24 @@
+import random
 def main():
-  #print('You rolled a die')
 
+  a=input("押大还是小：")
+  b=random.choice(range(1,7))
+  if (b<4) and (a=="小") or (b>=4) and (a=="大"):
+  print("数字是：",b)
+  print("恭喜您中了500万")
+else:
+  print("数字是：", b)
+  print("谢谢惠顾")
+  ! Wow that's a classic! You might be able to make a version of import random
+  a=input("押大还是小：")
+  b=random.choice(range(1,7))
+if (b<4) and (a=="小") or (b>=4) and (a=="大"):
+  print("数字是：",b)
+  print("恭喜您中了500万")
+else:
+  print("数字是：", b)
+  print("谢谢惠顾")
+  
+  
 if __name__== "__main__":
   main()
